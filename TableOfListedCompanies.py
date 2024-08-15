a7504917e2d0d67d9a74527c78b5697418eb5582
Diwad003 <48996015+Diwad003@users.noreply.github.com>
2024-08-15 21:12:04 +0200

    Future profed TableOfListedCompanies.. modifed gitignore

--- TableOfListedCompanies.py
+++ TableOfListedCompanies.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup as bs
+from io import StringIO
 
 headers = {
     'accept-language': 'en-US,en;q=0.9',
@@ -11,7 +12,7 @@ url = 'http://www.nasdaqomxnordic.com/shares/listed-companies/nordic-large-cap'
 
 r = requests.get(url, headers=headers)
 table = bs(r.text, 'html.parser').select_one('table#listedCompanies')
-df = pd.read_html(str(table))[0]
+df = pd.read_html(StringIO(str(table)))[0]
 
 df.to_csv('nordic_large_cap_companies.csv', index=False)
 print("DataFrame saved to nordic_large_cap_companies.csv")
